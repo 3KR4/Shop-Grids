1a9e8637b4b39b07a1106852f7b9a771e8a032de
3KR4 <mouhamedmahmoud820@gmail.com>
2023-09-23 07:21:39 +0200

    luffy

--- js/Header-Footer.js
+++ js/Header-Footer.js
@@ -1,265 +1,264 @@
-
-
+// //! sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss Header
 let header = document.querySelector(".header");
-mainHeader = () => {
-  return (header.innerHTML = `
-  <div class="nav-1">
-  <div class="container">
-    <div class="region">
-      <ul>
-        <li class="currency">
-          <select class="select-currency">
-            <option value="0">$ USD</option>
-            <option value="2">$ CAD</option>
-            <option value="3">₹ INR</option>
-            <option value="4">¥ CNY</option>
-            <option value="5">৳ BDT</option>
-          </select>
-        </li>
-        <li class="language">
-          <select class="select-language">
-            <option value="0">English</option>
-            <option value="1">Español</option>
-            <option value="2">Filipino</option>
-            <option value="3">Français</option>
-            <option value="4">العربية</option>
-            <option value="5">हिन्दी</option>
-            <option value="6">বাংলা</option>
-          </select>
-        </li>
-      </ul>
-    </div>
-  <div class="login">
-    <div class="hello">
-      <i class="bi bi-person"></i>
-      Hello
-    </div>
-    <div class="sign-in">
-      <a href="" class="login-border">Sign In</a>
-      <a href="">Register</a>
-    </div>
-  </div>
-</div>
-</div>
-<div class="container">
-<div class="nav-2">
-    <a class="logo" href="index.html">
-      <i class="bi bi-basket"></i>
-      Shop
-      <span>Grids</span> 
-    </a>
+// mainHeader = () => {
+//   return (header.innerHTML = `
+//   <div class="nav-1">
+//   <div class="container">
+//     <div class="region">
+//       <ul>
+//         <li class="currency">
+//           <select class="select-currency">
+//             <option value="0">$ USD</option>
+//             <option value="2">$ CAD</option>
+//             <option value="3">₹ INR</option>
+//             <option value="4">¥ CNY</option>
+//             <option value="5">৳ BDT</option>
+//           </select>
+//         </li>
+//         <li class="language">
+//           <select class="select-language">
+//             <option value="0">English</option>
+//             <option value="1">Español</option>
+//             <option value="2">Filipino</option>
+//             <option value="3">Français</option>
+//             <option value="4">العربية</option>
+//             <option value="5">हिन्दी</option>
+//             <option value="6">বাংলা</option>
+//           </select>
+//         </li>
+//       </ul>
+//     </div>
+//   <div class="login">
+//     <div class="hello">
+//       <i class="bi bi-person"></i>
+//       Hello
+//     </div>
+//     <div class="sign-in">
+//       <a href="" class="login-border">Sign In</a>
+//       <a href="">Register</a>
+//     </div>
+//   </div>
+// </div>
+// </div>
+// <div class="container">
+// <div class="nav-2">
+//     <a class="logo" href="index.html">
+//       <i class="bi bi-basket"></i>
+//       Shop
+//       <span>Grids</span> 
+//     </a>
 
-    <div class="holder">
-      <div class="searsh-content">
-        <select class="select-currency">
-          <option value="0">All</option>
-          <option value="2">option 01</option>
-          <option value="3">option 02</option>
-          <option value="4">option 03</option>
-          <option value="5">option 04</option>
-          <option value="5">option 05</option>
-        </select>
-        <input class=" search1 " type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-primary search" type="submit">
-        <i class="fa-solid fa-magnifying-glass "></i>
-        </button>
-      </div>
-      <div class="hot-line">
-        <i class="fa-solid fa-phone"></i>
-        <div>
-          Hotline:
-          <p>(+100) 123 456 7890</p>
-        </div>
-      </div>
-    </div>
+//     <div class="holder">
+//       <div class="searsh-content">
+//         <select class="select-currency">
+//           <option value="0">All</option>
+//           <option value="2">option 01</option>
+//           <option value="3">option 02</option>
+//           <option value="4">option 03</option>
+//           <option value="5">option 04</option>
+//           <option value="5">option 05</option>
+//         </select>
+//         <input class=" search1 " type="search" placeholder="Search" aria-label="Search">
+//         <button class="btn btn-primary search" type="submit">
+//         <i class="fa-solid fa-magnifying-glass"></i>
+//         </button>
+//       </div>
+//       <div class="hot-line">
+//         <i class="fa-solid fa-phone"></i>
+//         <div>
+//           Hotline:
+//           <p>(+100) 123 456 7890</p>
+//         </div>
+//       </div>
+//     </div>
 
-    <div class="products-holder">
-        <div class="darkLight darkLight1">
-          <i class="bi bi-brightness-high-fill sun"></i>
-          <i class="bi bi-moon-fill moon"></i>
-        </div>
+//     <div class="products-holder">
+//         <div class="darkLight darkLight1">
+//           <i class="bi bi-brightness-high-fill sun"></i>
+//           <i class="bi bi-moon-fill moon"></i>
+//         </div>
 
-      <i class="fa-solid fa-heart love">
-        <span>
-          0
-        </span>
-      </i>
-      <i class="fa-solid fa-cart-shopping cart">
-        <span>
-          2
-        </span>
-        <div class="main-cart">
-          <div class="item">
-            <h6>2 Items</h6>
-            <a href="">View Cart</a>
-          </div>
-          <div class="cards">
-            <div class="card">
-              <img src="img/item1.png" alt="">
-              <div class="info">
-                <h6>Apple Watch</h6>
-                <h6>Series 6</h6>
-                <p>1x - $35.00</p>
-              </div>
-              <i class="bi bi-x-lg"></i>
-            </div>
-            <div class="card">
-              <img src="img/item2.png" alt="">
-              <div class="info">
-                <h6>Wi-Fi Smart</h6>
-                <h6>Camera</h6>
-                <p>1x - $35.00</p>
-              </div>
-              <i class="bi bi-x-lg"></i>
-            </div>
-          </div>
-          <div class="total">
-            <h6>Total</h6>
-            <h6>$134.00</h6>
-          </div>
-          <a class="Check" href="">Check Out</a>
-        </div>
-      </i>
-    </div>
+//       <i class="fa-solid fa-heart love">
+//         <span>
+//           0
+//         </span>
+//       </i>
+//       <i class="fa-solid fa-cart-shopping cart">
+//         <span>
+//           2
+//         </span>
+//         <div class="main-cart">
+//           <div class="item">
+//             <h6>2 Items</h6>
+//             <a href="">View Cart</a>
+//           </div>
+//           <div class="cards">
+//             <div class="card">
+//               <img src="img/item1.png" alt="">
+//               <div class="info">
+//                 <h6>Apple Watch</h6>
+//                 <h6>Series 6</h6>
+//                 <p>1x - $35.00</p>
+//               </div>
+//               <i class="bi bi-x-lg"></i>
+//             </div>
+//             <div class="card">
+//               <img src="img/item2.png" alt="">
+//               <div class="info">
+//                 <h6>Wi-Fi Smart</h6>
+//                 <h6>Camera</h6>
+//                 <p>1x - $35.00</p>
+//               </div>
+//               <i class="bi bi-x-lg"></i>
+//             </div>
+//           </div>
+//           <div class="total">
+//             <h6>Total</h6>
+//             <h6>$134.00</h6>
+//           </div>
+//           <a class="Check main-buttom" href="">Check Out</a>
+//         </div>
+//       </i>
+//     </div>
 
-</div>
-<div class="nav-3">
-  <div class="categories">
-      <i class='bx bx-menu'></i>
-    All Categories 
-    <ul class="main-mega-menu">
-      <li><a href="">Electronics <i class="fa-solid fa-angle-right"></i></a>
-      <ul class="main-mega-menu-2">
-        <li><a href="">Camcorders</a></li>
-        <li><a href="">Camera Drones</a></li>
-        <li><a href="">Smart Watches</a></li>
-        <li><a href="">Headphones</a></li>
-        <li><a href="">MP3 Players</a></li>
-        <li><a href="">Microphones</a></li>
-        <li><a href="">Chargers</a></li>
-        <li><a href="">Batteries</a></li>
-        <li><a href="">Cables & Adapters</a></li>
-      </ul>
-      </li>
-      <li><a href="">Accessories</a></li>
-      <li><a href="">Televisions</a></li>
-      <li><a href="">best selling</a></li>
-      <li><a href="">top 100 offer</a></li>
-      <li><a href="">sunglass</a></li>
-      <li><a href="">watch</a></li>
-      <li><a href="">man’s product</a></li>
-      <li><a href="">Audio & Theater</a></li>
-      <li><a href="">Tablets</a></li>
-      <li><a href="">Video Games</a></li>
-      <li><a href="">Home Appliances</a></li>
-    </ul>
-  </div>
-  <ul class="nav-link">
-    <li><a href="index.html">Home</a></li>
-    <li onclick="megaClick1()"> <div class="title">Pages <i class="fa-solid fa-angle-down"></i></div> 
-    <ul class="mega mega-menu-1">
-      <li><a href="">About Us</a></li>
-      <li><a href="">Faq</a></li>
-      <li><a href="">Login</a></li>
-      <li><a href="">Register</a></li>
-      <li><a href="">Mail Success</a></li>
-      <li><a href="">404 Error</a></li>
-    </ul>
-    </li>
-    <li onclick="megaClick2()"><div class="title"> Shop <i class="fa-solid fa-angle-down"></i> </div>
-      <ul class="mega mega-menu-2">
-        <li><a href="">Shop Grid</a></li>
-        <li><a href="">Shop List</a></li>
-        <li><a href="">Shop Single</a></li>
-        <li><a href="">Cart</a></li>
-        <li><a href="">Checkout</a></li>
-      </ul>
-    </li>
-    <li onclick="megaClick3()"><div class="title">Blog <i class="fa-solid fa-angle-down"></i> </div>
-      <ul class="mega mega-menu-3">
-        <li> <a href="">Blog Grid Sidebar</a></li>
-        <li> <a href="">Blog Single</a></li>
-        <li><a href="">Blog Single Sibebar</a></li>
-      </ul>
-    </li>
-    <li><a href="">Contact Us</a></li>
-  </ul>
-  <span onclick="navLinkClick()" class="mainSpan">
-    <span class="bar1"></span>
-    <span class="bar2"></span>
-    <span class="bar3"></span>
-  </span>
-  <div class="follow">
-    <h3>Follow Us:</h3> 
-    <a href=""><i class="bi bi-facebook"></i></a>
-    <a href=""><i class="bi bi-twitter"></i></a>
-    <a href=""><i class="bi bi-messenger"></i></a>
-    <a href=""><i class="bi bi-discord discord"></i></a>
-  </div>
-  <div class="products-holder">
-  <div class="darkLight darkLight2">
-    <i class="bi bi-brightness-high-fill sun"></i>
-    <i class="bi bi-moon-fill moon"></i>
-  </div>
+// </div>
+// <div class="nav-3">
+//   <div class="categories">
+//       <i class='bx bx-menu'></i>
+//     All Categories 
+//     <ul class="main-mega-menu">
+//       <li><a href="">Electronics <i class="fa-solid fa-angle-right"></i></a>
+//       <ul class="main-mega-menu-2">
+//         <li><a href="">Camcorders</a></li>
+//         <li><a href="">Camera Drones</a></li>
+//         <li><a href="">Smart Watches</a></li>
+//         <li><a href="">Headphones</a></li>
+//         <li><a href="">MP3 Players</a></li>
+//         <li><a href="">Microphones</a></li>
+//         <li><a href="">Chargers</a></li>
+//         <li><a href="">Batteries</a></li>
+//         <li><a href="">Cables & Adapters</a></li>
+//       </ul>
+//       </li>
+//       <li><a href="">Accessories</a></li>
+//       <li><a href="">Televisions</a></li>
+//       <li><a href="">best selling</a></li>
+//       <li><a href="">top 100 offer</a></li>
+//       <li><a href="">sunglass</a></li>
+//       <li><a href="">watch</a></li>
+//       <li><a href="">man’s product</a></li>
+//       <li><a href="">Audio & Theater</a></li>
+//       <li><a href="">Tablets</a></li>
+//       <li><a href="">Video Games</a></li>
+//       <li><a href="">Home Appliances</a></li>
+//     </ul>
+//   </div>
+//   <ul class="nav-link">
+//     <li><a href="index.html">Home</a></li>
+//     <li onclick="megaClick1()"> <div class="title title-1">Pages <i class="fa-solid fa-angle-down angle-down1"></i></div> 
+//     <ul class="mega mega-menu-1">
+//       <li><a href="">About Us</a></li>
+//       <li><a href="">Faq</a></li>
+//       <li><a href="">Login</a></li>
+//       <li><a href="">Register</a></li>
+//       <li><a href="">Mail Success</a></li>
+//       <li><a href="">404 Error</a></li>
+//     </ul>
+//     </li>
+//     <li onclick="megaClick2()"><div class="title title-2"> Shop <i class="fa-solid fa-angle-down angle-down2"></i> </div>
+//       <ul class="mega mega-menu-2">
+//         <li><a href="">Shop Grid</a></li>
+//         <li><a href="">Shop List</a></li>
+//         <li><a href="">Shop Single</a></li>
+//         <li><a href="">Cart</a></li>
+//         <li><a href="">Checkout</a></li>
+//       </ul>
+//     </li>
+//     <li onclick="megaClick3()"><div class="title title-3">Blog <i class="fa-solid fa-angle-down angle-down3"></i> </div>
+//       <ul class="mega mega-menu-3">
+//         <li> <a href="">Blog Grid Sidebar</a></li>
+//         <li> <a href="">Blog Single</a></li>
+//         <li><a href="">Blog Single Sibebar</a></li>
+//       </ul>
+//     </li>
+//     <li><a href="">Contact Us</a></li>
+//   </ul>
+//   <span onclick="navLinkClick()" class="mainSpan">
+//     <span class="bar1"></span>
+//     <span class="bar2"></span>
+//     <span class="bar3"></span>
+//   </span>
+//   <div class="follow">
+//     <h3>Follow Us:</h3> 
+//     <a href=""><i class="bi bi-facebook"></i></a>
+//     <a href=""><i class="bi bi-twitter"></i></a>
+//     <a href=""><i class="bi bi-messenger"></i></a>
+//     <a href=""><i class="bi bi-discord discord"></i></a>
+//   </div>
+//   <div class="products-holder">
+//   <div class="darkLight darkLight2">
+//     <i class="bi bi-brightness-high-fill sun"></i>
+//     <i class="bi bi-moon-fill moon"></i>
+//   </div>
 
-<i class="fa-solid fa-heart love">
-  <span>
-    0
-  </span>
-</i>
-<i class="fa-solid fa-cart-shopping cart">
-  <span>
-    2
-  </span>
-  <div class="main-cart">
-    <div class="item">
-      <h6>2 Items</h6>
-      <a href="">View Cart</a>
-    </div>
-    <div class="cards">
-      <div class="card">
-        <img src="img/item1.png" alt="">
-        <div class="info">
-          <h6>Apple Watch</h6>
-          <h6>Series 6</h6>
-          <p>1x - $35.00</p>
-        </div>
-        <i class="bi bi-x-lg"></i>
-      </div>
-      <div class="card">
-        <img src="img/item2.png" alt="">
-        <div class="info">
-          <h6>Wi-Fi Smart</h6>
-          <h6>Camera</h6>
-          <p>1x - $35.00</p>
-        </div>
-        <i class="bi bi-x-lg"></i>
-      </div>
-    </div>
-    <div class="total">
-      <h6>Total</h6>
-      <h6>$134.00</h6>
-    </div>
-    <a class="Check" href="">Check Out</a>
-  </div>
-</i>
-</div>
-</div>
-<div class="nav-4">
-  <h4 class="pageName1"></h4>
-  <div class="pages">
-    <div class="home">
-      <i class="bi bi-house-door"></i>
-      <a href="index.html">Home</a>
-    </div>
-    <i class="fa-solid fa-angle-right"></i>
-    <h6 class="pageName2"></h6>
-  </div>
-</div>
-</div>
-  `);
-};
-mainHeader();
+// <i class="fa-solid fa-heart love">
+//   <span>
+//     0
+//   </span>
+// </i>
+// <i class="fa-solid fa-cart-shopping cart">
+//   <span>
+//     2
+//   </span>
+//   <div class="main-cart">
+//     <div class="item">
+//       <h6>2 Items</h6>
+//       <a href="">View Cart</a>
+//     </div>
+//     <div class="cards">
+//       <div class="card">
+//         <img src="img/item1.png" alt="">
+//         <div class="info">
+//           <h6>Apple Watch</h6>
+//           <h6>Series 6</h6>
+//           <p>1x - $35.00</p>
+//         </div>
+//         <i class="bi bi-x-lg"></i>
+//       </div>
+//       <div class="card">
+//         <img src="img/item2.png" alt="">
+//         <div class="info">
+//           <h6>Wi-Fi Smart</h6>
+//           <h6>Camera</h6>
+//           <p>1x - $35.00</p>
+//         </div>
+//         <i class="bi bi-x-lg"></i>
+//       </div>
+//     </div>
+//     <div class="total">
+//       <h6>Total</h6>
+//       <h6>$134.00</h6>
+//     </div>
+//     <a class="Check main-buttom" href="">Check Out</a>
+//   </div>
+// </i>
+// </div>
+// </div>
+// <div class="nav-4">
+//   <h4 class="pageName1"></h4>
+//   <div class="pages">
+//     <div class="home">
+//       <i class="bi bi-house-door"></i>
+//       <a href="index.html">Home</a>
+//     </div>
+//     <i class="fa-solid fa-angle-right"></i>
+//     <h6 class="pageName2"></h6>
+//   </div>
+// </div>
+// </div>
+//   `);
+// };
+// mainHeader();
 
 let body = document.querySelector("body");
 modeToggle1 = document.querySelector(".darkLight1");
@@ -315,6 +314,40 @@ function navLinkClick () {
   bar3.classList.toggle("active")
 }
 
+let themBtn1 = document.querySelector(".them1")
+let themBtn2 = document.querySelector(".them2")
+let themBtn3 = document.querySelector(".them3btn")
+let themico3 = document.querySelector(".them3")
+let themHolder = document.querySelector(".them-holder")
+
+function nagi2 (themsBtn) {
+  themsBtn.addEventListener("click", () => {
+    themHolder.classList.toggle("active")
+    if (themHolder.classList.contains("active")) {
+      themico3.classList.replace("fa-circle-half-stroke","fa-x")
+    } else {
+      themico3.classList.replace("fa-x","fa-circle-half-stroke")
+    }
+  })
+}
+nagi2(themBtn1)
+nagi2(themBtn2)
+nagi2(themBtn3)
+
+let themeButtons = document.querySelectorAll(".theme-buttons")
+let themBlue = document.querySelectorAll(".themBlue")
+
+themeButtons.forEach(color =>{
+  color.addEventListener('click', () => {
+  let dataColor = color.getAttribute('data-color');
+  document.querySelector(':root').style.setProperty('--main-color', dataColor);
+  });
+});
+
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 225);
+});
+
 function pageNameChanger1 () {
   let pageName = document.querySelector(".pageName1")
   pageName.innerHTML = document.title
@@ -327,7 +360,21 @@ function pageNameChanger2 () {
 }
 pageNameChanger2()
 
+//! ScrollBar / States
+const scrollBar = document.querySelector(".scrollBar");
 
-window.addEventListener("scroll", function () {
-  header.classList.toggle("sticky", window.scrollY > 155);
-});
+window.onscroll = function () {
+  if(this.scrollY >= 400){
+    scrollBar.classList.add('show')
+  }else{
+    scrollBar.classList.remove('show')
+  }
+};
+scrollBar.onclick = function () {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
--- js/master.js
+++ js/master.js
@@ -1,3 +1,4 @@
+//! sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss categories
 const categories = [
   {
     mainName: "Mother Board",
@@ -72,10 +73,10 @@ const categories = [
     img:"img/phone.png"
   }
   ];
-  let testCards = document.querySelector(".categories .container")
+  let categoriesCards = document.querySelector(".categories .container")
   
   function rednderCategories() {
-  let testItems = categories.map((item) => {
+  let categoriesItems = categories.map((item) => {
   
     return`
       <div class="card">
@@ -93,6 +94,300 @@ const categories = [
       </div>
     `
   }).join("") 
-  testCards.innerHTML = testItems
+  categoriesCards.innerHTML = categoriesItems
   };
-  rednderCategories();
+rednderCategories();
+
+//! sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss Product
+
+const Product = [
+{
+  img:"img/product-1.png",
+  type: "Watches",
+  name:"Xiaomi Mi Band 5",
+  review:"40",
+  price:"$199.00",
+  price2:"",
+  info:"",
+  infoClass:"",
+  star1:"fa-solid fa-star",
+  star2:"fa-regular fa-star",
+  star3:"fa-regular fa-star",
+},
+{
+  img:"img/product-2.png",
+  type: "Speaker",
+  name:"Big Power Sound Speaker",
+  review:"50",
+  price:"$275.00 ",
+  price2:"$300.00",
+  info:"-25%",
+  infoClass:"red",
+  star1:"fa-solid fa-star",
+  star2:"fa-solid fa-star-half-stroke",
+  star3:"fa-regular fa-star",
+},
+{
+  img:"img/product-3.png",
+  type: "Camera",
+  name:"WiFi Security Camera",
+  review:"36",
+  price:"$399.00",
+  price2:"",
+  info:"",
+  infoClass:"",
+  star1:"fa-solid fa-star",
+  star2:"fa-regular fa-star",
+  star3:"fa-regular fa-star",
+},
+{
+  img:"img/product-4.png",
+  type: "Phones",
+  name:"iphone 6x plus",
+  review:"67",
+  price:"$400.00",
+  price2:"",
+  info:"new",
+  infoClass:"blue",
+  star1:"fa-solid fa-star",
+  star2:"fa-solid fa-star",
+  star3:"fa-regular fa-star",
+},
+{
+  img:"img/product-6.png",
+  type: "Speaker",
+  name:"Mini Bluetooth Speaker",
+  review:"22",
+  price:"$70.00",
+  price2:"",
+  info:"",
+  infoClass:"",
+  star1:"fa-solid fa-star",
+  star2:"fa-solid fa-star-half-stroke",
+  star3:"fa-regular fa-star",
+},
+{
+  img:"img/product-5.png",
+  type: "Headphones",
+  name:"Wireless Headphones",
+  review:"27",
+  price:"$350.00",
+  price2:"",
+  info:"",
+  infoClass:"",
+  star1:"fa-solid fa-star",
+  star2:"fa-regular fa-star",
+  star3:"fa-regular fa-star",
+},
+{
+  img:"img/product-7.png",
+  type: "Headphones",
+  name:"PX7 Wireless Headphones",
+  review:"86",
+  price:"$100.00 ",
+  price2:"$300.00",
+  info:"-50%",
+  infoClass:"red",
+  star1:"fa-solid fa-star",
+  star2:"fa-regular fa-star",
+  star3:"fa-regular fa-star",
+},
+{
+  img:"img/product-8.png",
+  type: "Laptop",
+  name:"Apple MacBook Air",
+  review:"14",
+  price:"$899.00",
+  price2:"",
+  info:"",
+  infoClass:"",
+  star1:"fa-solid fa-star",
+  star2:"fa-solid fa-star",
+  star3:" fa-regular fa-star",
+}
+];
+
+let productCards = document.querySelector(".Product .container")
+
+function rednderProduct() {
+let productItems = Product.map((item) => {
+
+  return`
+    <div class="main-card">
+      <div class="image">
+        <img src="${item.img}" alt="">
+        <a class="main-buttom" href="">
+          <i class="bi bi-cart2"></i>
+          Add To Cart
+        </a>
+        <span class="${item.infoClass}">${item.info}</span>
+      </div>
+      <div class="holder">
+        <p>${item.type}</p>
+        <h3>${item.name}</h3>
+        <div class="review">
+          <div class="stars">
+            <i class="${item.star1}"></i>
+            <i class="${item.star1}"></i>
+            <i class="${item.star1}"></i>
+            <i class="${item.star2}"></i>
+            <i class="${item.star3}"></i>
+          </div>
+          <p>${item.review} Review</p>
+        </div>
+        <h4>${item.price} <p>${item.price2}</p></h4>
+      </div>
+    </div>
+  `
+  }).join("") 
+  productCards.innerHTML = productItems
+  };
+rednderProduct();
+//! sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss offer
+
+const offer = [
+{
+  img:"img/offer_1.png",
+  type: "Mouse",
+  name:"XPG INFAREX M10",
+  review:"68",
+  price:"$18.00",
+  price2:"$20.00",
+  info:"",
+  infoClass:"",
+  star1:"fa-solid fa-star",
+  star2:"fa-solid fa-star",
+  star3:"fa-solid fa-star",
+},
+{
+  img:"img/offer_2.png",
+  type: "Headphones",
+  name:"Logitech G PRO X",
+  review:"50",
+  price:"$180.00 ",
+  price2:"$200.00",
+  info:"",
+  infoClass:"",
+  star1:"fa-solid fa-star",
+  star2:"fa-solid fa-star",
+  star3:"fa-regular fa-star",
+},
+{
+  img:"img/offer_3.png",
+  type: "Memory",
+  name:"XPG D60G RGB 16GB",
+  review:"36",
+  price:"$60.00",
+  price2:"$85.00",
+  info:"",
+  infoClass:"",
+  star1:"fa-solid fa-star",
+  star2:"fa-solid fa-star",
+  star3:"fa-solid fa-star",
+},
+{
+  img:"img/offer_4.png",
+  type: "Mother Board",
+  name:"Asus Rog Strix Z790",
+  review:"12",
+  price:"$350.00",
+  price2:"$500.00",
+  info:"",
+  infoClass:"",
+  star1:"fa-solid fa-star",
+  star2:"fa-regular fa-star",
+  star3:"fa-regular fa-star",
+},
+{
+  img:"img/offer_5.png",
+  type: "Procesor",
+  name:"Intel i9-12900KS",
+  review:"3",
+  price:"$799.00",
+  price2:"$840.00",
+  info:"",
+  infoClass:"",
+  star1:"fa-solid fa-star",
+  star2:"fa-regular fa-star",
+  star3:"fa-regular fa-star",
+},
+{
+  img:"img/offer_6.png",
+  type: "Monitor",
+  name:"Msi G244F24 170HZ",
+  review:"23",
+  price:"$205.00",
+  price2:"$220.00",
+  info:"",
+  infoClass:"",
+  star1:"fa-solid fa-star",
+  star2:"fa-solid fa-star",
+  star3:"fa-regular fa-star",
+},
+];
+
+let offerCards = document.querySelector(".offer .bigHolder .products")
+
+function rednderoffer() {
+let offerItems = offer.map((item) => {
+
+  return`
+    <div class="main-card">
+      <div class="image">
+        <img src="${item.img}" alt="">
+        <a class="main-buttom" href="">
+          <i class="bi bi-cart2"></i>
+          Add To Cart
+        </a>
+        <span class="${item.infoClass}">${item.info}</span>
+      </div>
+      <div class="holder">
+        <p>${item.type}</p>
+        <h3>${item.name}</h3>
+        <div class="review">
+          <div class="stars">
+            <i class="${item.star1}"></i>
+            <i class="${item.star1}"></i>
+            <i class="${item.star1}"></i>
+            <i class="${item.star2}"></i>
+            <i class="${item.star3}"></i>
+          </div>
+          <p>${item.review} Review</p>
+        </div>
+        <h4>${item.price} <p>${item.price2}</p></h4>
+      </div>
+    </div>
+  `
+  }).join("") 
+  offerCards.innerHTML = offerItems
+  };
+rednderoffer();
+
+// counter
+
+let countDownDate = new Date("Sep 30, 2023 23:59:59").getTime();
+// console.log(countDownDate);
+
+let counter = setInterval(() => {
+  // Get Date Now
+  let dateNow = new Date().getTime();
+
+  // Find The Date Difference Between Now And Countdown Date
+  let dateDiff = countDownDate - dateNow;
+
+  // Get Time Units
+  // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+  document.querySelector(".days span").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".hours span").innerHTML = hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".minutes span").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds span").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+
+  if (dateDiff < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
